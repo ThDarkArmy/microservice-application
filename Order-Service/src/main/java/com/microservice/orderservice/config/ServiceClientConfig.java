82a52df142ca692fb8d2089e1fc5939d24c01691
Prashant Raj <darkarmy@Prashants-MacBook-Pro.local>
2026-09-04 21:32:24 +0530

    eureka server and api gateway

--- Order-Service/src/main/java/com/microservice/orderservice/config/ServiceClientConfig.java
+++ Order-Service/src/main/java/com/microservice/orderservice/config/ServiceClientConfig.java
@@ -4,20 +4,39 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class ServiceClientConfig {
 
     @Bean
+    @LoadBalanced
+    @Qualifier("loadBalancedRestClientBuilder")
+    RestClient.Builder loadBalancedRestClientBuilder() {
+        return RestClient.builder();
+    }
+
+    @Bean
+    @Primary
+    RestClient.Builder eurekaRestClientBuilder() {
+        return RestClient.builder();
+    }
+
+    @Bean
     @Qualifier("inventoryClient")
-    RestClient inventoryClient(@Value("${inventory.service.url}") String inventoryServiceUrl) {
-        return RestClient.builder().baseUrl(inventoryServiceUrl).build();
+    RestClient inventoryClient(
+            @Qualifier("loadBalancedRestClientBuilder") RestClient.Builder restClientBuilder,
+            @Value("${inventory.service.url}") String inventoryServiceUrl) {
+        return restClientBuilder.baseUrl(inventoryServiceUrl).build();
     }
 
     @Bean
     @Qualifier("notificationClient")
-    RestClient notificationClient(@Value("${notification.service.url}") String notificationServiceUrl) {
-        return RestClient.builder().baseUrl(notificationServiceUrl).build();
+    RestClient notificationClient(
+            @Qualifier("loadBalancedRestClientBuilder") RestClient.Builder restClientBuilder,
+            @Value("${notification.service.url}") String notificationServiceUrl) {
+        return restClientBuilder.baseUrl(notificationServiceUrl).build();
     }
 }
